29b34bed3ca253a105c678f0d9a0b1d70a3e4164
Roh <82229769+rohyunsang@users.noreply.github.com>
2025-09-02 01:48:07 +0900

    Initial commit

---
+++ Source/SplitScreen/SplitScreen.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SplitScreen : ModuleRules
+{
+	public SplitScreen(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(new string[] { "SplitScreen" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
