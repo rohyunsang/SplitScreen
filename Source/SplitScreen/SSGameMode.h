6ee33873422cb6591a06e3a74dd295f539ab48ee
Roh <82229769+rohyunsang@users.noreply.github.com>
2025-09-24 04:06:22 +0900

    Revert "작업중"
    This reverts commit a187de418f2e7edbf5ff80002a0b7ba2477f598b.

--- Source/SplitScreen/SSGameMode.h
+++ Source/SplitScreen/SSGameMode.h
@@ -26,6 +26,9 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Split Screen")
     TSubclassOf<APawn> DummySpectatorPawnClass;
 
+protected:
+    UFUNCTION(BlueprintCallable, Category = "Split Screen")
+    void SetupOnlineSplitScreen();
 
     UFUNCTION(BlueprintCallable, Category = "Split Screen")
     void UpdateSplitScreenLayout();
@@ -35,6 +38,8 @@ private:
     class ASSDummySpectatorPawn* DummySpectatorPawn;
     class ASSPlayerController* DummyPlayerController;
 
+    void CreateDummyLocalPlayer();
+    void SyncDummyPlayerWithRemotePlayer();
 
     FTimerHandle SyncTimerHandle;
 
